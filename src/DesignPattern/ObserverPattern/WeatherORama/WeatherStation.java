84e8d8464d06645bc3e2e3b37e3535cac20f3663
Vivek Kumar <vivekkr0311@gmail.com>
2025-12-26 14:00:30 +0530

    [Observer Pattern]: New observers add aka displays

--- src/DesignPattern/ObserverPattern/WeatherORama/WeatherStation.java
+++ src/DesignPattern/ObserverPattern/WeatherORama/WeatherStation.java
@@ -1,12 +1,16 @@
 package DesignPattern.ObserverPattern.WeatherORama;
 
 import DesignPattern.ObserverPattern.WeatherORama.Observers.CurrentConditionsDisplay;
+import DesignPattern.ObserverPattern.WeatherORama.Observers.ForecastDisplay;
+import DesignPattern.ObserverPattern.WeatherORama.Observers.StatisticsDisplay;
 import DesignPattern.ObserverPattern.WeatherORama.Subjects.WeatherData;
 
 public class WeatherStation {
     public static void main(String[] args) {
         WeatherData weatherData = new WeatherData();
         CurrentConditionsDisplay currentConditionsDisplay = new CurrentConditionsDisplay(weatherData);
+        StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
+        ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
 
         weatherData.setMeasurements(80, 65, 30.4f);
         weatherData.setMeasurements(82, 70, 29.2f);
